c13b261928ce322133b43af4d47f1bcfd2a7de76
Christopher Rush <ceo@christopherhustle.com>
2022-10-30 17:36:32 +0100

    project init

---
+++ src/main/java/com/hastesoft/users/UsersApiClientApplication.java
@@ -0,0 +1,13 @@
+package com.hastesoft.users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersApiClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsersApiClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hastesoft/users/UsersApiClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hastesoft.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersApiClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
